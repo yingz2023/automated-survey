3c48514371fb1a8aa9c696c81772768df0c9e6ea
yingz2023 <smilesmilemegan@gmail.com>
2023-06-19 16:49:05 -0400

    Simplify report.R

--- actions/report.R
+++ actions/report.R
@@ -15,37 +15,8 @@ timestamps <- basename(data_directory)
 latest_timestamp <- max(timestamps)
 latest_directory <- file.path("data/device-testing", latest_timestamp)
 responses_filepath <- file.path(latest_directory, "Responses.csv")
-labels_filepath <- file.path(latest_directory, "Labels.csv")
 
 responses <- read_csv(responses_filepath)
-labels <- read_csv(labels_filepath)
 
-# Create a data frame for mapping the variable_name and variable_label
-colname_mapping <- left_join(
-  tibble(old_names = names(responses)),
-  labels, 
-  by = c("old_names" = "variable_label")
-)
-
-# Handle columns not found in labels
-colname_mapping$variable_name <- ifelse(
-  is.na(colname_mapping$variable_name),
-  colname_mapping$old_names,
-  colname_mapping$variable_name
-)
-
-# Set the new names to responses data frame
-names(responses) <- colname_mapping$variable_name
-labelled::var_label(responses) <- labels |> pull(variable_label)
-
-responses <- responses %>% mutate(across(q1:q10, 
-                                         ~ case_when(
-                                           . == "Strongly disagree" ~ 1,
-                                           . == "Somewhat disagree" ~ 2,
-                                           . == "Neither disagree nor agree" ~ 3,
-                                           . == "Somewhat agree" ~ 4,
-                                           . == "Strongly agree" ~ 5,
-                                           TRUE ~ NA_real_
-                                         ), .names = "{.col}"))
 
 write.csv(responses, "data/survey_responses.csv")
